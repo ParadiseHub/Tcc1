c4d3860c64478a07950aa71b11361030b5be2475
ParadiseHub <160505534+ParadiseHub@users.noreply.github.com>
2024-06-10 15:58:31 -0300

    Add files via upload

---
+++ tcc/js/grafico1.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('barchart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [2, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
